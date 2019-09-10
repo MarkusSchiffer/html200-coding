48de5d1672cd8826a77cfccee987e786729629c7
Markus Schiffer <47703802+MarkusSchiffer@users.noreply.github.com>
2019-09-10 14:29:46 -0700

    created the calculator.js file

---
+++ lesson02/calculator.js
@@ -0,0 +1,16 @@
+const str = '42';
+const int = 7;
+const float = 5.5;
+const zero = 0;
+
+console.log(str / int);
+console.log(int ** int);
+console.log(float - int);
+console.log(str * float);
+console.log(int / zero);
+console.log(float * zero);
+console.log(int < zero);
+console.log(str * zero);
+console.log(str + zero);
+
+
